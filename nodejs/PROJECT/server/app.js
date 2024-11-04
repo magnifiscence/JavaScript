fef2eb64ecfbad9a26387a90fb0b06ad43b6ff05
tata <tdivine284@gmail.com>
2024-11-04 17:01:47 +0100

    node app

--- nodejs/PROJECT/server/app.js
+++ nodejs/PROJECT/server/app.js
@@ -12,16 +12,40 @@ app.use(express.urlencoded({ extended : false }));
 
 // create
 app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
 
 });
 // read
 app.get('/getAll', (request, response) => {
-    response.json({
-        success: true
-    }); 
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+     
 })
 // update  
 
 // delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+    .then(data => response.json({success : true}))
+    .catch(err => console.log(err));
+
+});
 
 app.listen(process.env.PORT, () => console.log('app is running'));
--- nodejs/PROJECT/server/dbService.js
+++ nodejs/PROJECT/server/dbService.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
+let instance = null;
 dotenv.config();
 
 const connection = mysql.createConnection({
@@ -7,12 +8,89 @@ const connection = mysql.createConnection({
     user: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.BD_PORT
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
     if (err) {
         console.log(err.message);
     }
-    console.log('db ' + connection.state);
-})
+   // console.log('db ' + connection.state);
+});
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+
+            const query = "SELECT * FROM names";
+
+            connection.query(query, (err, results) => {
+                if (err) reject(new Error(err.message));
+                resolve(results);
+            })
+        });
+
+        //console.log(response);
+        return response;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+
+                const query = "INSERT INTO names (name, date_added) VALUES (?, ?);";
+    
+                connection.query(query, [name, dateAdded], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results.insertId);
+                })
+            });
+            
+            return {
+                id : insertId,
+                name : name,
+                dateAdded : dateAdded
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10);
+
+            const response = await new Promise((resolve, reject) => {
+
+                const query = "DELETE FROMnames WHERE id = ?";
+
+                connection.query(query, [name, dateAdded], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results.affectedRows);
+                })
+            });
+
+            return response === 1 ? true : false;
+
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+
+        
+
+        console.log(response);
+    }
+}
+
+module.exports = DbService;
